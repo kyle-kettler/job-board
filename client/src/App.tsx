8f8843bd9f4c3ab97650fbf4c6c62d35a1d20e08
Kyle Kettler <hello@kylekettler.com>
2023-07-27 17:30:14 +0000

    updated server to use TS and installed tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>{serverData}</h1>
+        <h1 className="text-3xl font-bold underline">{serverData}</h1>
       </header>
     </div>
   );
