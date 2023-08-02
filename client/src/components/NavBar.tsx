059d9ea43268a0f41cc3f68a498a5046ea0710f6
Kyle Kettler <hello@kylekettler.com>
2023-08-02 22:45:03 +0000

    updated NavBar design

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -13,17 +13,18 @@ export default function NavBar() {
           </div>
           <ul className="flex items-center gap-4">
             <li>
-              <Link to="/jobs">Jobs</Link>
+              <Link to="/">Explore Jobs</Link>
             </li>
             <li>
-              <Link to="/saved">Saved</Link>
-            </li>
-            <li>
-              <Link to="/applied">Applied</Link>
+              <Link to="/sign-in">
+                <button className="text-base  px-3 border-[1.5px] border-stone-700 rounded hover:bg-stone-300 transition-colors">
+                  Sign In
+                </button>
+              </Link>
             </li>
             <li>
               <Link to="/sign-up">
-                <button className="bg-orange-500 hover:bg-orange-600 text-white py-1 px-3 rounded transition-colors text-base">
+                <button className="px-3 bg-orange-500 border-[1.5px] border-orange-500 hover:border-orange-600 hover:bg-orange-600 text-white rounded transition-colors text-base">
                   Sign Up
                 </button>
               </Link>
